76c2bf1ed0e88f57f2e1f4d082d707320b8fce95
Suraj Yadav <155947736+SurajYadav8@users.noreply.github.com>
2024-01-08 09:18:59 +0000

    container config done

---
+++ 01_Basic/DataTypes.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// alert("Hello")
+
+let name = "hitesh"
+let age = 18
+let isloggedIn = false
+
+// number => 2 to  power 53
+// bigint 
+// string => ""
+// Boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object 
+
+console.log(typeof "Suraj");
---
+++ 01_Basic/Variables.js
@@ -0,0 +1,14 @@
+const account_ID = "11425"
+let account_Email = "Sy@google.com"
+var accountPassword = "14569"
+accountCity = "Jaipur"
+
+// account_ID = 2
+ 
+console.log(account_ID)
+
+account_Email = "SS@Gmail.com"
+accountPassword = "5546"
+accountCity = "Delhi"
+
+console.table([accountPassword,account_Email,account_ID,accountCity])
