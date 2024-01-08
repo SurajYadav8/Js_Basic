6f53fda19ceab2e464f528eceb1fcfad8af03394
Suraj Yadav <155947736+SurajYadav8@users.noreply.github.com>
2024-01-08 09:57:32 +0000

    conversion of Datatypes

--- 01_Basic/conversionoperation.js
+++ 01_Basic/conversionoperation.js
@@ -1,1 +1,28 @@
-let scroe = 33
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =. 0
+
+let isloggedIn = "Suraj"
+
+let BooleanIsloggedIn = Boolean(isloggedIn)
+// console.log(BooleanIsloggedIn);
+
+// 1 => true; 0 => false
+//  "" => false
+//  "Suraj" => true
+
+let someNumber = 33 
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
