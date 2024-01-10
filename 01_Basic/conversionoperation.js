17e68e5a65a859c0f84b866f44a91a4530c10aba
Suraj Yadav <155947736+SurajYadav8@users.noreply.github.com>
2024-01-10 11:28:46 +0000

    conversion

--- 01_Basic/conversionoperation.js
+++ 01_Basic/conversionoperation.js
@@ -23,6 +23,26 @@ let BooleanIsloggedIn = Boolean(isloggedIn)
 let someNumber = 33 
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+
+let value = 3 
+let negvalue = -value
+// console.log(negvalue);
+
+let str1 = "Suraj"
+let str2 = " Yadav"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// // if the string is first in the given operation then the no of whole opertation will treat as string 
+// console.log((3+4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
