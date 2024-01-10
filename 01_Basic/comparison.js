a060a46971eef35f188235b06b64791049b0e670
Suraj Yadav <155947736+SurajYadav8@users.noreply.github.com>
2024-01-10 11:29:10 +0000

    comparison

---
+++ 01_Basic/comparison.js
@@ -0,0 +1,13 @@
+// comparison which occurs problem
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === strict check
+
+console.log("2" === 2);
+// checks datatypes also
