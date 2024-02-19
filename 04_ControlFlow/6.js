78c80725a9bb61a13aee39cf559315069018e7b3
Suraj Yadav <155947736+SurajYadav8@users.noreply.github.com>
2024-02-19 15:28:23 +0000

    reduce

--- 04_ControlFlow/6.js
+++ 04_ControlFlow/6.js
@@ -1,6 +1,12 @@
 // reduce method 
 const Nums = [1,2,3]
 
-myTotal = myNums.reduce()
+const myTotal = Nums.reduce(function (acc,currval) {
+    console.log(`acc: ${acc} and currval: ${currval}`);
+    return acc + currval
+},0)
 
+console.log(myTotal);
 
+const myttl = mynm.reducer( (acc, curr) => acc+curr,0)
+console.log(myttl);
